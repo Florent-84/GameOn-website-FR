334123802639a3cd190d12cd55cdc2065e723d05
Florent-84 <florent.tuppo@live.fr>
2021-10-25 16:42:05 +0200

    fermeture modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,33 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+//création de la constante closeBtn
+const closeBtn = document.getElementsByClassName("close");
 
-// launch modal event
+
+
+// événement lancement de modal
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+// lancer le formulaire modal
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+
+
+// fermerture du formulaire // 
+
+//création de la fonction
+function closeModal() {
+  modalbg.style.display = "none";
+}
+//écouter l'événement et au click fermer la modale 
+closeBtn[0].addEventListener("click", closeModal);
+
+
+
+
+
 
 
